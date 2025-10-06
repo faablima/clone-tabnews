5e489e67d8e5791ef2f179085a92b5040412295d
Fábio Lima <fabiogomeslma@gmail.com>
2025-10-06 18:25:14 +0000

    adicionado pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+        <div>
+            <h1>Frase Impactante</h1>
+            <p>Os desejos dominantes podem ser cristalizados nos seus equivalentes físicos, através de definição de propósito</p>
+        </div>
+    );
+}
+
+export default Home;
