617f967a8ad40b10bbd5ccf8c767ce6928a84462
Fabio Gomes de Lima <fabiogomeslma@gmail.com>
2025-10-18 08:18:10 -0300

    fix: `getSSLValues()` function return

--- infra/database.js
+++ infra/database.js
@@ -36,5 +36,5 @@ function getSSLValues() {
       rejectUnauthorized: true,
     };
   }
-  process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "development" ? false : true;
 }
